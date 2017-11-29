26fd469182d1fe2b97bf61aaff04b5a0c569c450
claramarshall <claramarshall@rogers.com>
2017-11-29 15:42:17 -0500

    created header and footer partials

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,60 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@include('partials.head')
 
-        <title>{{$name}} I am</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 24px;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
         <div class="flex-center position-ref full-height">
 
             <div class="content">
@@ -67,5 +12,7 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
+
+
+@include('partials.footer')
+
