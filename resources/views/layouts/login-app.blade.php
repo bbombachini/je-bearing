9d6a92b950109cee171ed21e62c2fc6a384dad9e
Barbara Bombachini <bbombachini@gmail.com>
2018-01-24 08:49:07 -0500

    Add login-app view

---
+++ resources/views/layouts/login-app.blade.php
@@ -0,0 +1,24 @@
+  @include('partials.head')
+  <!-- CONTENT STARTS HERE -->
+
+  <div id="container-login">
+      <section id="welcome-con">
+        <div id="welcome-text">
+          <h1>Hello</h1>
+        </div>
+
+        <div id="logout">
+      		<img src="../../../images/white-arrow.png" alt="left arrow" id="leftarrow">
+      		<p>LOGOUT</p>
+      	</div>
+
+      </section>
+
+
+      <!-- @yield('content') -->
+      <section id="context">
+        <h2>Another hello</h2>
+      </section>
+
+
+  @include('partials.footer')
